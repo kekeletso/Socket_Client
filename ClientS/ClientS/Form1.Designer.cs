e1cf4f67f8c24af7ad3052afff5439280e415a63
kekeletso <kekeletso@live.co.uk>
2017-11-09 08:34:53 +0200

    Client

---
+++ ClientS/ClientS/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ClientS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtuserip = new System.Windows.Forms.TextBox();
+            this.btnaddress = new System.Windows.Forms.Button();
+            this.txtresponse = new System.Windows.Forms.TextBox();
+            this.lblmsg = new System.Windows.Forms.Label();
+            this.lblip = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+           
+            this.SuspendLayout();
+            // 
+            // txtuserip
+            // 
+            this.txtuserip.Location = new System.Drawing.Point(127, 27);
+            this.txtuserip.Name = "txtuserip";
+            this.txtuserip.Size = new System.Drawing.Size(115, 20);
+            this.txtuserip.TabIndex = 0;
+            // 
+            // btnaddress
+            // 
+            this.btnaddress.Location = new System.Drawing.Point(127, 94);
+            this.btnaddress.Name = "btnaddress";
+            this.btnaddress.Size = new System.Drawing.Size(115, 23);
+            this.btnaddress.TabIndex = 1;
+            this.btnaddress.Text = "click to ping address";
+            this.btnaddress.UseVisualStyleBackColor = true;
+            this.btnaddress.Click += new System.EventHandler(this.btnaddress_Click);
+            // 
+            // txtresponse
+            // 
+            this.txtresponse.Location = new System.Drawing.Point(127, 160);
+            this.txtresponse.Name = "txtresponse";
+            this.txtresponse.Size = new System.Drawing.Size(115, 20);
+            this.txtresponse.TabIndex = 2;
+            // 
+            // lblmsg
+            // 
+            this.lblmsg.AutoSize = true;
+            this.lblmsg.Location = new System.Drawing.Point(330, 199);
+            this.lblmsg.Name = "lblmsg";
+            this.lblmsg.Size = new System.Drawing.Size(0, 13);
+            this.lblmsg.TabIndex = 3;
+            // 
+            // lblip
+            // 
+            this.lblip.AutoSize = true;
+            this.lblip.Location = new System.Drawing.Point(29, 34);
+            this.lblip.Name = "lblip";
+            this.lblip.Size = new System.Drawing.Size(85, 13);
+            this.lblip.TabIndex = 4;
+            this.lblip.Text = "Enter IP address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Server Response";
+            // 
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 263);
+            
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblip);
+            this.Controls.Add(this.lblmsg);
+            this.Controls.Add(this.txtresponse);
+            this.Controls.Add(this.btnaddress);
+            this.Controls.Add(this.txtuserip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtuserip;
+        private System.Windows.Forms.Button btnaddress;
+        private System.Windows.Forms.TextBox txtresponse;
+        private System.Windows.Forms.Label lblmsg;
+        private System.Windows.Forms.Label lblip;
+        private System.Windows.Forms.Label label1;
+     
+    }
+}
+
---
+++ ClientS/ClientS/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ClientS
+{
+    public partial class Form1 : Form
+    {
+       
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+
+        public Form1()
+        {
+
+            try
+            {
+                // initialize the form
+                InitializeComponent();
+
+                // connect to the server address
+                clientSocket.Connect("127.0.0.1", 8080);
+
+                //show that its connected
+                lblmsg.Text = "Client Socket Program - Server Connected ..."; 
+                
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Connection error.." + ex);
+            }
+
+        }
+
+      
+        private async void btnaddress_Click(object sender, EventArgs e)
+        {
+
+            //to be used for the ping
+            Ping aPing = new Ping();  
+           // txtresponse.Text = "";
+
+            try
+            {
+               
+                //clear the txtresponse field
+                txtresponse.Text = ""; 
+                                
+                //connect to the server
+                NetworkStream serverStream = clientSocket.GetStream(); 
+
+                // get ip address from textbox and parse it
+                IPAddress ipAddress = IPAddress.Parse(txtuserip.Text); 
+
+                // create a buffer for transmitting data
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(".");
+
+                // time to wait for reply
+                int timeout = 10000;
+
+                // set data gateways
+                PingOptions options = new PingOptions(64, true); 
+
+                //send the ping asynchronously
+                //assign response to variable
+                var reply = await aPing.SendPingAsync(ipAddress, timeout, outStream, options);
+
+                //check response 
+                //assign approriate response to textbox
+                if(reply.Status == IPStatus.Success)
+                {
+
+                    txtresponse.Text = "Successful";
+                }
+                else
+                {
+
+                    txtresponse.Text = "Failed";
+                }
+
+
+                
+            }
+            catch (PingException ex)
+            {
+               //MessageBox.Show("opps" , "error");
+               throw new Exception("Ping error.." + ex);
+
+            }
+            
+        }
+
+       
+
+        
+    }
+}
